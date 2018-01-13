073e618c01ca892563213fb4fd7a3eb1aed0f3ee
Greg Rose <gregrose628@gmail.com>
2018-01-13 11:49:30 -0500

    added MovieThumb component, added movies to discover page

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -1,11 +1,67 @@
 import React, { Component } from 'react';
+import './Discover.css';
+import authApi from '../api/authApi';
+import MovieThumb from './MovieThumb';
 
 class Discover extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/movies/discover', {
+      headers: new Headers({
+      'Authorization': authApi.getToken(),
+      'Content-Type': 'application/json'
+      })
+    })
+      .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          let errorMessage = `${response.status} (${response.statusText})`,
+              error = new Error(errorMessage);
+          throw(error);
+        }
+      })
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          page: response.page,
+          movies: response.results,
+          totalPages: response.total_pages
+        });
+      })
+      .catch(error => console.error(`Error in fetch: ${error.message}`));
+  }
 
   render() {
+    let movies = this.state.movies.map(movieData => {
+      return (
+        <MovieThumb
+          key={movieData.id}
+          data={movieData}
+        />
+      )
+    })
+
+    if (movies.length === 0) {
+      movies = (
+        <div className="col">
+          <i className="fa fa-spinner" aria-hidden="true"></i>
+        </div>
+      )
+    }
+
     return (
-      <div>
-        <h1>Discover</h1>
+      <div className="row text-center">
+        <div className="col-12">
+          <h1 id="discover-title">Discover</h1>
+        </div>
+        {movies}
       </div>
     );
   }
---
+++ src/components/MovieThumb.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './MovieThumb.css';
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
+
+class MovieThumb extends Component {
+  render() {
+    let poster = `https://image.tmdb.org/t/p/w154/${this.props.data.poster_path}`;
+
+    return (
+      <div className="col-6 col-md-4 col-lg-3">
+        <Card className="mx-auto movie-card">
+          <CardImg top src={poster} />
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default MovieThumb;
