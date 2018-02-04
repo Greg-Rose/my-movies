d7427cedc3d1c8b684e02ce8b94be7c0308d7bca
Greg Rose <gregrose628@gmail.com>
2018-02-04 01:43:29 -0500

    added genres filter to discover component

--- src/components/browse/Discover.js
+++ src/components/browse/Discover.js
@@ -3,16 +3,42 @@ import './Discover.css';
 import authApi from '../../api/authApi';
 import MovieThumb from '../movie/MovieThumb';
 import { API_ROOT } from '../../api/apiConfig';
+import { FormGroup, Label, Input } from 'reactstrap';
 
 class Discover extends Component {
   constructor() {
     super();
     this.state = {
-      movies: []
+      movies: [],
+      genres: []
     };
+    this.selectGenre = this.selectGenre.bind(this);
   }
 
   componentDidMount() {
+    fetch(API_ROOT + '/genres', {
+      headers: new Headers({
+      'Authorization': authApi.getToken(),
+      'Content-Type': 'application/json'
+      })
+    })
+      .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          let errorMessage = `${response.status} (${response.statusText})`,
+              error = new Error(errorMessage);
+          throw(error);
+        }
+      })
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          genres: response
+        });
+      })
+      .catch(error => console.error(`Error in fetch: ${error.message}`));
+
     fetch(API_ROOT + '/movies/discover', {
       headers: new Headers({
       'Authorization': authApi.getToken(),
@@ -39,7 +65,41 @@ class Discover extends Component {
       .catch(error => console.error(`Error in fetch: ${error.message}`));
   }
 
+  selectGenre(event) {
+    let genre = event.target.options[event.target.selectedIndex].getAttribute("data-tmdb-id");
+
+    fetch(API_ROOT + '/movies/discover?genres=' + genre, {
+      headers: new Headers({
+        'Authorization': authApi.getToken(),
+        'Content-Type': 'application/json'
+      })
+    })
+      .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          let errorMessage = `${response.status} (${response.statusText})`,
+              error = new Error(errorMessage);
+          throw(error);
+        }
+      })
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          page: response.page,
+          movies: response.results,
+          totalPages: response.total_pages
+        });
+      })
+      .catch(error => console.error(`Error in fetch: ${error.message}`));
+  }
+
   render() {
+    let genres = this.state.genres.map(genre => {
+      return (
+        <option key={genre.tmdb_id} data-tmdb-id={genre.tmdb_id}>{genre.name}</option>
+      )
+    });
     let movies = this.state.movies.map(movieData => {
       return (
         <MovieThumb
@@ -60,6 +120,15 @@ class Discover extends Component {
 
     return (
       <div className="row text-center">
+        <div className="col-12">
+          <FormGroup>
+            <Label for="genres">Genres</Label>
+            <Input type="select" name="select" id="genres" onChange={this.selectGenre}>
+              <option data-tmdb-id="">All</option>
+              {genres}
+            </Input>
+          </FormGroup>
+        </div>
         {movies}
       </div>
     );
