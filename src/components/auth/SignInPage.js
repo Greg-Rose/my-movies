21b54e312b134701638ffd54bd0901d1d9791451
Greg Rose <gregrose628@gmail.com>
2018-02-13 23:54:08 -0500

    added alert message to sign in page

--- src/components/auth/SignInPage.js
+++ src/components/auth/SignInPage.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './SignInPage.css';
-import { Button, Form, FormGroup, Input } from 'reactstrap';
+import { Button, Form, FormGroup, Input, Alert } from 'reactstrap';
 import authApi from '../../api/authApi';
 import { NavLink } from 'react-router-dom';
 
 class SignInPage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      alert: false,
+      email: '',
+      password: ''
+    };
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
@@ -26,13 +31,22 @@ class SignInPage extends Component {
       password: this.state.password
     };
 
-    authApi.signIn(credentials)
-      .then(() => this.redirect());
+    if (credentials.email.trim().length === 0 || credentials.password.trim().length === 0 ) {
+      this.setState({ alert: "Email and password can't be blank" });
+    }
+    else {
+      this.setState({ alert: false });
+      authApi.signIn(credentials)
+        .then(() => this.redirect());
+    }
   }
 
   redirect() {
     if(authApi.userSignedIn()) {
       this.props.history.replace('/');
+    }
+    else if (this.state.email.trim().length >= 1 || this.state.password.trim().length >= 1) {
+      this.setState({ alert: "Email and password are incorrect" });
     }
   }
 
@@ -42,6 +56,7 @@ class SignInPage extends Component {
         <div className="col col-sm-8 col-lg-4">
           <Form id="sign-in-form" onSubmit={this.onSubmit}>
             <h2 className="text-center">Sign In</h2>
+            <Alert color="danger" isOpen={!!this.state.alert}>{this.state.alert}</Alert>
             <FormGroup>
               <Input type="email" name="email" id="email" placeholder="Email" onChange={this.onChange} />
             </FormGroup>
