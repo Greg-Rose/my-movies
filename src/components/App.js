a23cf6f59080e29619d41fa3986713556e012d88
Greg Rose <gregrose628@gmail.com>
2018-03-05 23:27:56 -0500

    moved withAuth logic into App component and removed withAuth

--- src/components/App.js
+++ src/components/App.js
@@ -5,28 +5,51 @@ import SignInPage from './auth/SignInPage';
 import SignUpPage from './auth/SignUpPage';
 import { Route, Switch } from 'react-router-dom';
 import Browse from './browse/Browse';
-import withAuth from './auth/withAuth';
 import NoMatch from './auth/NoMatch';
 import MyMovies from './user/MyMovies';
 import Search from './browse/Search';
 import Account from './user/Account';
+import authApi from '../api/authApi';
+import NavBar from './layout/NavBar';
 
 class App extends Component {
+  componentWillMount() {
+    if (!authApi.userSignedIn() && this.props.history.location.pathname !== '/sign_up') {
+      this.props.history.replace('/sign_in');
+    }
+    else {
+      let token = authApi.getToken();
+      if (authApi.isTokenExpired(token)) {
+        authApi.signOut();
+        this.props.history.replace('/sign_in' && this.props.history.location.pathname !== '/sign_up');
+      }
+    }
+  }
+
   render() {
+    let navbar;
+
+    if (authApi.userSignedIn()) {
+      navbar = <NavBar history={this.props.history} />
+    }
+
     return (
-      <Container>
-        <Switch>
-          <Route exact path="/sign_in" component={SignInPage} />
-          <Route exact path="/sign_up" component={SignUpPage} />
-          <Route path='/search' component={Search} />
-          <Route path='/browse' component={Browse} />
-          <Route path='/my_movies' component={MyMovies} />
-          <Route exact path='/my_account' component={Account} />
-          <Route component={NoMatch} />
-        </Switch>
-      </Container>
+      <div>
+        {navbar}
+        <Container>
+          <Switch>
+            <Route exact path="/sign_in" component={SignInPage} />
+            <Route exact path="/sign_up" component={SignUpPage} />
+            <Route path='/search' component={Search} />
+            <Route path='/browse' component={Browse} />
+            <Route path='/my_movies' component={MyMovies} />
+            <Route exact path='/my_account' component={Account} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Container>
+      </div>
     );
   }
 }
 
-export default withAuth(App);
+export default App;
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -3,14 +3,38 @@ import {shallow} from 'enzyme';
 import App from './App';
 import { Container } from 'reactstrap';
 import { Route, Switch } from 'react-router-dom';
-import withAuth from './auth/withAuth';
-jest.mock('./auth/withAuth');
+import authApi from '../api/authApi';
+import NavBar from './layout/NavBar';
+import { createMemoryHistory } from 'history';
 
 describe('<App />', () => {
   let wrapper;
+  let authApiSpy;
 
   beforeEach(() => {
-    wrapper = shallow(<App />);
+    authApiSpy = jest.spyOn(authApi, 'signOut')
+      .mockImplementation(() => {
+        return null;
+      });
+
+    const authApiUserSignedInSpy = jest.spyOn(authApi, 'userSignedIn')
+      .mockImplementation(() => {
+        return true;
+      });
+
+    const authApiIsTokenExpiredSpy = jest.spyOn(authApi, 'isTokenExpired')
+      .mockImplementation(() => {
+        return false;
+      });
+
+    const authApiGetTokenSpy = jest.spyOn(authApi, 'getToken')
+      .mockImplementation(() => {
+        return "token";
+      });
+
+    const history = createMemoryHistory('/');
+
+    wrapper = shallow(<App history={history} />);
   });
 
   it('should render a Container component', () => {
@@ -23,5 +47,34 @@ describe('<App />', () => {
 
   it('should render 7 Route components', () => {
     expect(wrapper.find(Route).length).toEqual(7);
+  });
+
+  it('should render NavBar component if user is signed in', () => {
+    expect(wrapper.find(NavBar).exists()).toEqual(true);
+  });
+
+  it('should not render NavBar component if user is not signed in', () => {
+    const authApiUserSignedInSpy = jest.spyOn(authApi, 'userSignedIn')
+      .mockImplementation(() => {
+        return false;
+      });
+
+    const history = createMemoryHistory('/');
+
+    wrapper = shallow(<App history={history} />);
+    expect(wrapper.find(NavBar).exists()).toEqual(false);
+  });
+
+  it('should sign out if token is expired', () => {
+    const authApiIsTokenExpiredSpy = jest.spyOn(authApi, 'isTokenExpired')
+      .mockImplementation(() => {
+        return true;
+      });
+
+    const history = createMemoryHistory('/');
+
+    wrapper = shallow(<App history={history} />);
+
+    expect(authApiSpy).toHaveBeenCalled();
   });
 });
